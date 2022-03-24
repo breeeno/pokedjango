dcf37f0863160bd3642bd695b0898c512e5744b3
Walison Filipe <walisonfilipe@hotmail.com>
2022-03-23 23:30:32 -0300

    Testa criacao de pokemons

--- pokemon/models.py
+++ pokemon/models.py
@@ -4,8 +4,8 @@ from tipo.models import Tipo
 
 class Pokemon(models.Model):
     nome = models.CharField(max_length=50)
-    elemento = models.ManyToManyField(Tipo)
     sprite = models.ImageField(upload_to='sprites', null=True, blank=True)
+    elemento = models.ManyToManyField(Tipo)
 
     def __str__(self):
         return self.nome

367bf5799ab65733e7915a12a06afb2c4e8e113b
Breno <bx.sptf@gmail.com>
2022-03-29 20:59:41 -0300

    - Tentativa de fix na busca por imagem

--- pokemon/models.py
+++ pokemon/models.py
@@ -5,7 +5,7 @@ from tipo.models import Elemento
 class Pokemon(models.Model):
     nome = models.CharField(max_length=50)
     elemento = models.ManyToManyField(Elemento)
-    sprite = models.ImageField(upload_to='sprites', null=True, blank=True)
+    sprite = models.ImageField(upload_to='static/sprites/', null=True, blank=True)
 
     def __str__(self):
         return self.nome

486533c7374e00cbfcf974bb5544747e41ed29e1
Breno <bx.sptf@gmail.com>
2022-03-29 02:36:40 -0300

    - Implementado diversas melhorias e o postgres como db

--- pokemon/api/serializer.py
+++ pokemon/api/serializer.py
@@ -1,8 +1,11 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from pokemon.models import Pokemon
 
 
 class PokemonSerializer(ModelSerializer):
+    elemento = serializers.StringRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Pokemon
         fields = ['id', 'nome', 'elemento', 'sprite']

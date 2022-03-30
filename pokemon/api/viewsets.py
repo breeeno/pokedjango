6a44fb2c713bc15dc105b3528e2cb02c25f0d834
Breno <bx.sptf@gmail.com>
2022-03-30 00:31:49 -0300

    - Melhoria na filtragem de pkms

--- pokemon/api/viewsets.py
+++ pokemon/api/viewsets.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from pokemon.api.serializer import PokemonSerializer
 from pokemon.models import Pokemon
+from rest_framework import filters
 
 
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['nome']

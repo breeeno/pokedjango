5a0f84ccd48d93b17c865305966ee3b259e90f9f
Breno <bx.sptf@gmail.com>
2022-04-01 14:04:59 -0300

    fix no serializer

--- pokemon/api/serializer.py
+++ pokemon/api/serializer.py
@@ -4,7 +4,7 @@ from pokemon.models import Pokemon
 
 
 class PokemonSerializer(ModelSerializer):
-    elemento = serializers.StringRelatedField(many=True)
+    elemento = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Pokemon
